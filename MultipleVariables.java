9e33afdd83a1dcbefb1e2e321563e5f2aadcafbe
Sahith Sai Pasupula <sahithsaipasupula@gmail.com>
2026-07-20 19:23:16 +0530

    Add files via upload

---
+++ MultipleVariables.java
@@ -0,0 +1,14 @@
+public class MultipleVariables {
+    public static void main(String[] args){
+        String name= "Sahith Sai";
+        int age =19;
+        String college = "SIMATS ENGINEERING";
+        String department= "AIML";
+        char grade ='A';
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("College:" + college);
+        System.out.println("Department:" + department);
+        System.out.println("Grade:" + grade);
+      }
+}
