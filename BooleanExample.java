d321a06454596c0df295fc3ac4d2a99256cd8f81
Sahith Sai Pasupula <sahithsaipasupula@gmail.com>
2026-07-20 19:12:10 +0530

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,7 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+
+        boolean passed = false;
+        System.out.println(passed);
+    }
+}
---
+++ LongExample.java
@@ -0,0 +1,6 @@
+public class LongExample {
+    public static void main(String[] args){
+        long mobile = 8639712707L;
+    System.out.println(mobile);
+    }
+}
